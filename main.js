3a12de4f03c3fddec9c5779c1546d139e38c4f1f
gabodani <153448099+gabodani@users.noreply.github.com>
2024-04-23 21:40:52 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-let product=document.querySelector(".wooden");
+let product=document.querySelector(".used_air");
 product.addEventListener("click",function(){
     window.open("second.html","_parent")
-})
+}) 
+let producttwo=document.querySelector(".used_airtwo");
+producttwo.addEventListener("click",function(){
+    window.open("second.html","_parent")
+}) 
+let productthree=document.querySelector(".used_airthree");
+productthree.addEventListener("click",function(){
+    window.open("second.html","_parent")
+}) 
